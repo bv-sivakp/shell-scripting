eceebf73e237bb5d54764cecd302ee2ac55a33f2
bv-sivakp <61418764+bv-sivakp@users.noreply.github.com>
2021-02-06 13:34:03 +0530

    print array with index

--- arrays.sh
+++ arrays.sh
@@ -5,9 +5,14 @@ declare -A my_array=(["firstname"]="john"
                 ["show"]="GameOfThrones"
                 )
 
-for value in "${my_array[@]}"; 
-do 
- echo "$value"; 
+for my_array_index in ${!my_array[@]}
+do
+  echo $my_array_index  ${my_array[$my_array_index]}
+  # if [ $my_array_index == "show" ]
+  # then
+  #  SMOKETEST_TAG_NAME=${MS_TAG_NAMES[$MS_TAG_NAME]}
+  #  break
+  # fi
 done
 
 
